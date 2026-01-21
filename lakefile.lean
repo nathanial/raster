e9177b453eff762fb487374e69c12e3fb6913e85
Nathanial Hartman <nathanial.hartman@gmail.com>
2026-01-21 06:11:27 -0800

    Update crucible to v0.0.8

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package raster where
   version := v!"0.1.0"
   precompileModules := true
 
-require crucible from git "https://github.com/nathanial/crucible" @ "v0.0.7"
+require crucible from git "https://github.com/nathanial/crucible" @ "v0.0.8"
 require staple from git "https://github.com/nathanial/staple" @ "v0.0.2"
 
 @[default_target]
